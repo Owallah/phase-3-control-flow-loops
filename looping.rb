3657b70f167f12e4039f65ccc9412c9b8c354b81
Owallah <mikesonowallah@gmail.com>
2023-05-16 17:26:43 +0300

    Completed assignment

--- looping.rb
+++ looping.rb
@@ -1,6 +1,34 @@
+
+# function happyNewYear() {
+#   let counter = 10;
+#   while (counter > 0) {
+#     console.log(counter);
+#     counter--;
+#   }
+#   console.log("Happy New Year!");
+# }
+
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
+
+# function fizzbuzz(num) {
+#   if (num % 3 === 0 && num % 5 === 0) {
+#     return "FizzBuzz";
+#   } else if (num % 3 === 0) {
+#     return "Fizz";
+#   } else if (num % 5 === 0) {
+#     return "Buzz";
+#   } else {
+#     return num;
+#   }
+# }
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +43,34 @@ def fizzbuzz(num)
   end
 end
 
+# function fizzbuzzPrinter() {
+#   for (let num = 1; num <= 100; num++) {
+#     console.log(fizzbuzz(num));
+#   }
+# }
+
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+    end
 end
+
+# function reverseString(str) {
+#   let reversedStr = "";
+#   for (let i = 0; i < str.length; i++) {
+#     reversedStr = str[i] + reversedStr;
+#   }
+#   return reversedStr;
+# }
 
 def reverse_string(str)
   # your code here
+  reversedStr = ''
+
+  (str.length).times do |i|
+    reversedStr = str[i] + reversedStr
+  end
+  return reversedStr
 end
+
